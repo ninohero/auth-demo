546449a0f4a12020217eb02cc18e464d2e08f75f
ninohero <ninohero@muzocalo.com>
2019-06-06 16:12:34 -0400

    initial advanced redux auth tut commit

---
+++ src/components/SignOut.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SignOut extends Component {
+  render() {
+    return (
+      <div>Sorry to see you go</div>
+    );
+  }
+}
+
+export default SignOut;
